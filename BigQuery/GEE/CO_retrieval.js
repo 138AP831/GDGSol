3bdb32bfa09799cda8900b8cfcb5b086cb327cfe
138AP831 <peruvel.anushka@gmail.com>
2025-03-28 20:34:38 +0530

    Create CO_retrieval.js

---
+++ BigQuery/GEE/CO_retrieval.js
@@ -0,0 +1,86 @@
+var adminBoundaries = ee.FeatureCollection("FAO/GAUL/2015/level1");
+Map.addLayer(adminBoundaries, {}, 'Boundaries', false);
+
+// Function to handle user click and extract CO data
+var handleClick = function(coords) {
+  var pointGeometry = ee.Geometry.Point(coords.lon, coords.lat);
+  Map.centerObject(pointGeometry, 10);
+  Map.addLayer(pointGeometry, {color: 'red'}, 'Selected Location');
+
+  var analysisStartYear = '2020';
+  var analysisEndYear = '2024';
+
+  // Sentinel-5P image collection for CO
+  var sentinel5PCO = ee.ImageCollection("COPERNICUS/S5P/OFFL/L3_CO")
+    .select(['CO_column_number_density'], ['CO'])
+    .filterDate(analysisStartYear, analysisEndYear)
+    .filterBounds(pointGeometry);
+
+  var monthRange = ee.List.sequence(1, 12);
+
+  var monthlyCOData = ee.ImageCollection(
+    ee.List.sequence(
+      ee.Number.parse(analysisStartYear), 
+      ee.Number.parse(analysisEndYear).subtract(1)
+    ).map(function(year) {
+      return monthRange.map(function(month) {
+        var monthlyImage = sentinel5PCO
+          .filter(ee.Filter.calendarRange(year, year, 'year'))
+          .filter(ee.Filter.calendarRange(month, month, 'month'))
+          .mean();
+
+        var date = ee.Date.fromYMD(year, month, 1);
+        return monthlyImage
+          .set('system:time_start', date.millis())
+          .set('system:index', date.format('YYYY-MM-dd'));
+      });
+    }).flatten()
+  );
+
+  var monthlyCOAtPoint = monthlyCOData.map(function(image) {
+    var COValue = image.reduceRegion({
+      reducer: ee.Reducer.mean(),
+      geometry: pointGeometry,
+      scale: 3000,
+      maxPixels: 1e8
+    }).get('CO');
+
+    return image.set({
+      'CO_value': COValue,
+      'longitude': coords.lon,
+      'latitude': coords.lat
+    });
+  });
+
+  var COFeatureCollection = monthlyCOAtPoint.map(function(image) {
+    return ee.Feature(null, {
+      'date': image.get('system:index'),
+      'CO_value': image.get('CO_value'),
+      'longitude': image.get('longitude'),
+      'latitude': image.get('latitude')
+    });
+  });
+
+  // Exporting the results as CSV and GeoJSON
+  Export.table.toDrive({
+    collection: COFeatureCollection,
+    description: 'CO_Emissions_CSV',
+    fileFormat: 'CSV',
+    folder: 'Gas_Monitoring',
+    fileNamePrefix: 'CO_Emissions'
+  });
+
+  Export.table.toDrive({
+    collection: COFeatureCollection,
+    description: 'CO_Emissions_JSON',
+    fileFormat: 'GeoJSON',
+    folder: 'Gas_Monitoring',
+    fileNamePrefix: 'CO_Emissions'
+  });
+
+};
+
+// Capture click event on the map
+Map.onClick(handleClick);
+
+Map.setCenter(0, 0, 2);
